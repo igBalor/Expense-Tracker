dbc2abec86082e2e3cbbb587ba461bc25be9ed3b
Binay Kumar Patro <108986996+igBalor@users.noreply.github.com>
2024-03-31 04:38:58 +0530

    Add files via upload

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,31 @@
+// rrd imports
+import { Outlet, useLoaderData } from "react-router-dom";
+
+import wave from "../assets/wave.svg";    
+
+import Nav from "../components/Nav";
+            
+
+//  helper functions
+import { fetchData } from "../helpers"
+
+// loader
+export function mainLoader() {
+  const userName = fetchData("userName");
+  return { userName }
+}
+
+const Main = () => {
+  const { userName } = useLoaderData()
+
+  return (
+    <div className="layout">
+        <Nav userName={userName} />
+        <main>
+            <Outlet />
+        </main>
+        <img src={wave} alt="" />
+    </div>
+  )
+}
+export default Main
